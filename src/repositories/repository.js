2fd26559df840ac394673152c7ffd04b672bcfcc
nathanon58 <nathanon58@gmail.com>
2023-12-18 09:49:09 +0700

    apiurl

--- src/repositories/repository.js
+++ src/repositories/repository.js
@@ -1,7 +1,7 @@
 import { decodeToken } from "react-jwt";
 
-// export const API_URL = `https://ploisharedapi.ninenap.com/api/`;
-export const API_URL = `https://localhost:7111/api/`;
+export const API_URL = `https://ploisharedapi.ninenap.com/api/`;
+// export const API_URL = `https://localhost:7111/api/`;
 export const authUser = getAuth();
 function getAuth() {
   const token = localStorage.getItem("user");
@@ -9,4 +9,5 @@ function getAuth() {
   return JSON.parse(decode);
 }
 
-export const BASE_URL = `https://localhost:7111`;
+// export const BASE_URL = `https://localhost:7111`;
+export const BASE_URL = `https://ploisharedapi.ninenap.com`;
