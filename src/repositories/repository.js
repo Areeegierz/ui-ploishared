9d35fca5d684e8d704b445e4f3ea277d5c421184
Areeegierz <72206123+Areeegierz@users.noreply.github.com>
2023-12-19 13:55:12 +0700

    deploy19122023

--- src/repositories/repository.js
+++ src/repositories/repository.js
@@ -1,13 +1,18 @@
 import { decodeToken } from "react-jwt";
 
-export const API_URL = `https://ploisharedapi.ninenap.com/api/`;
-// export const API_URL = `https://localhost:7111/api/`;
+var mode = "deploy";
+export const API_URL =
+  mode === "dev"
+    ? `https://localhost:7111/api/`
+    : `http://110.170.56.74:9000/api/`;
+export const BASE_URL =
+  mode === "dev"
+    ? `https://localhost:7111/api/`
+    : `http://110.170.56.74:9000/api/`;
+
 export const authUser = getAuth();
 function getAuth() {
   const token = localStorage.getItem("user");
   const decode = decodeToken(token);
   return JSON.parse(decode);
 }
-
-// export const BASE_URL = `https://localhost:7111`;
-export const BASE_URL = `https://ploisharedapi.ninenap.com`;
